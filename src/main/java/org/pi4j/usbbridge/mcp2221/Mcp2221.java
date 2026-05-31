c8eed050081a4bfdd15db3fddcd23c5dc8d2b4e1
Stefan Haustein <stefan.haustein@gmail.com>
2026-05-31 22:21:04 +0200

    cleanup

--- src/main/java/org/pi4j/usbbridge/mcp2221/Mcp2221.java
+++ src/main/java/org/pi4j/usbbridge/mcp2221/Mcp2221.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import org.hid4java.*;
 import com.pi4j.io.SerialCircuitIO;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Mcp2221 {
     final HidDevice device;
 
@@ -44,47 +42,4 @@ public class Mcp2221 {
         return new Mcp2221I2c(this, address);
     }
 
-
-
-/*
-
-public static UsbAlternateInterface selectHid(UsbDevice device) {
-        for (UsbInterface iface : device.getInterfaces()) {
-            System.out.println("- Interface: " + iface);
-            for (UsbAlternateInterface alt : iface.getAlternates()) {
-                System.out.println("  - Alternate: " + alt + " class code: " + alt.getClassCode());
-                if (alt.getClassCode() == 3) {
-                    device.claimInterface(iface.getNumber());
-                    device.selectAlternateSetting(iface.getNumber(), alt.getNumber());
-                    return alt;
-                }
-            }
-        }
-        throw new IllegalArgumentException("No HID interface found");
-    }
-
-
-    public static void main(String[] args) {
-        var device = Usb.findDevice(0x04d8, 0x00dd).orElseThrow();
-
-        System.out.println("Device: " + device + " class code: " + device.getClassCode() + " interfaces: " + device.getInterfaces());
-
-        device.open();
-        UsbAlternateInterface hid = selectHid(device);
-
-        for (UsbEndpoint endpoint : hid.getEndpoints()) {
-            System.out.println("- endpoint: " + endpoint + " #" + endpoint.getNumber() + " dir: " + endpoint.getDirection()  + " packetSize: " + endpoint.getPacketSize() + " transferType: " + endpoint.getTransferType());
-        }
-
-
-        byte[] data = new byte[64];
-        data[0] = 0x10;
-
-        device.transferOut(3, data);
-        byte[] result = device.transferIn(3);
-
-        System.out.println("Transfer result: " + Arrays.toString(result));
-
-    }
-*/
 }
