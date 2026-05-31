7d291fa59f813ff72b4edf7008540d4869af7115
Stefan Haustein <stefan.haustein@gmail.com>
2026-05-31 22:18:13 +0200

    move the demo to tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("net.codecrete.usb:java-does-usb:1.2.1")
+//    implementation("net.codecrete.usb:java-does-usb:1.2.1")
     implementation("org.hid4java:hid4java:0.8.0")
     implementation("com.pi4j:pi4j-core:4.0.1")
     implementation("com.pi4j:pi4j-plugin-ffm:4.0.1")
